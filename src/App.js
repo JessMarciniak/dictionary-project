d06ccd5bc800e8bfa114e94c54f148a43bf55464
JessMarciniak <92031714+JessMarciniak@users.noreply.github.com>
2021-12-22 12:21:21 -0500

    Created search initial search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header text-center mt-5">Hello World!</header>
+      <div className="container">
+        <header className="App-header text-center mt-5">Hello World!</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by Jessica Marciniak</footer>
+      </div>
     </div>
   );
 }
