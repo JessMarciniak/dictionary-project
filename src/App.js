f46a1448c1e3b86cba5b46779f4af38224c790f5
JessMarciniak <92031714+JessMarciniak@users.noreply.github.com>
2021-12-22 11:48:17 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header text-center">Hello World!</header>
+    </div>
+  );
+}
+
+export default App;
