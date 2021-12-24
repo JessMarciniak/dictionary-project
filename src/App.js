efd8a11cd425d549b5c71a6d463a403f55931445
JessMarciniak <92031714+JessMarciniak@users.noreply.github.com>
2021-12-24 12:47:40 -0500

    Added GitHub link to footer

--- src/App.js
+++ src/App.js
@@ -5,11 +5,24 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header text-center mt-5">Hello World!</header>
+        <header className="App-header text-center mt-5">
+          React Dictionary App
+        </header>
         <main>
           <Dictionary />
         </main>
-        <footer className="App-footer">Coded by Jessica Marciniak</footer>
+        <footer className="App-footer">
+          Coded by Jessica Marciniak
+          <br />
+          Open-source code available on{" "}
+          <a
+            href="https://github.com/JessMarciniak/dictionary-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </footer>
       </div>
     </div>
   );
