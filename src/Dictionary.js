f4b6739cb22b13671498fb80634c6220f5a9c9fc
JessMarciniak <92031714+JessMarciniak@users.noreply.github.com>
2022-01-06 12:43:04 -0500

    Removed console log & added Phonetic component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -8,14 +8,12 @@ export default function Dictionary() {
   let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data[0].meanings[0].definitions[0].definition);
     setResults(response.data[0]);
   }
 
   function search(event) {
     event.preventDefault();
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-    console.log(apiUrl);
     axios.get(apiUrl).then(handleResponse);
   }
 
